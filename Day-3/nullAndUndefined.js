1cd2bf9dbab108eaf686bac0ddd566734f716d5c
Sanjeev <coolviccy123@gmail.com>
2024-08-14 17:41:39 +0530

    Null and Undefined Data type updated

--- Day-3/nullAndUndefined.js
+++ Day-3/nullAndUndefined.js
@@ -9,4 +9,12 @@
 // console.log(typeof /'100')
 // SyntaxError: Invalid regular expression: missing /
 
- 
+//  console.log(+'sanjeev') NaN
+//  console.log(+'100abcdef') NaN
+//  console.log(parseInt('100abcdef')) 100
+//  console.log(parseInt('100abcdef10efj')) 
+number = 100 + ''
+// console.log(typeof number)
+number = 100 + 'sanjeev'
+
+console.log(number)
