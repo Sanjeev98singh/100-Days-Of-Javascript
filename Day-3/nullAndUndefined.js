77cbc7b1962e66b1c0928dc83779ee14d00e0a37
Sanjeev <coolviccy123@gmail.com>
2024-08-14 17:32:02 +0530

    Null and Undefined Data type updated

--- Day-3/nullAndUndefined.js
+++ Day-3/nullAndUndefined.js
@@ -1,4 +1,12 @@
-console.log(typeof  'sanjeev')
-console.log(typeof  `sanjeev singh
-    bhadouria`)
+// console.log(typeof 'sanjeev')
+// console.log(typeof `sanjeev singh
+//     bhadouria`)
+// console.log(typeof 100)
+// console.log(typeof +'100')
+// console.log(typeof -'100')
+// // console.log(typeof *'100')
+// // SyntaxError: Unexpected token '*'
+// console.log(typeof /'100')
+// SyntaxError: Invalid regular expression: missing /
+
  
