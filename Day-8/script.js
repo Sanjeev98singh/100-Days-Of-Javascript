cc4cf0cb336e96765e198b9a2ebde33ab3e3d2c4
Sanjeev <coolviccy123@gmail.com>
2024-08-26 18:17:19 +0530

    trimStart() updated

--- Day-8/script.js
+++ Day-8/script.js
@@ -7,5 +7,11 @@
 
 // console.log(name.toLowerCase())
 
+// let name = '        kya kroge janke?        '
+// console.log(name.trim())
+
+// let name = '        kya kroge janke?      asas  '
+// console.log(name.trimStart().toUpperCase())
+
 let name = '        kya kroge janke?        '
 console.log(name.trim())
