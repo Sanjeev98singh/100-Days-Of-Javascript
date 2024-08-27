a7f963c7ac27046464e1332ba8c6dd03c477bea4
Sanjeev <coolviccy123@gmail.com>
2024-08-27 23:53:04 +0530

    math objects added

--- Day-9/script.js
+++ Day-9/script.js
@@ -1,1 +1,11 @@
 // console.log(Math.PI)
+// console.log(Math.SQRT2)
+// const min = 25;
+// const max = 50;
+// const randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
+// console.log(randomInteger);
+
+
+// let result = "hello" - 5;
+
+// console.log(result);
