82f5a0817af3d7e5bf3d1c8357d12b72673b584d
Sanjeev <coolviccy123@gmail.com>
2024-08-31 21:48:25 +0530

    Boolean check file added

--- Day-_10/script.js
+++ Day-_10/script.js
@@ -1,7 +1,11 @@
 // console.log(Boolean(10)) true
-console.log(Boolean(0.000000009))
-console.log(Boolean(-Infinity))
-console.log(Boolean(NaN))
-console.log(Boolean(''))
-console.log(Boolean(undefined))
-console.log(Boolean(null))
+// console.log(Boolean(0.000000009)) true
+// console.log(Boolean(-Infinity)) true
+// console.log(Boolean(NaN)) false
+// console.log(Boolean('')) false
+// console.log(Boolean(undefined)) false
+// console.log(Boolean(null)) false
+
+// console.log(Boolean(0)); false
+// console.log(Boolean(-0)); false
+
