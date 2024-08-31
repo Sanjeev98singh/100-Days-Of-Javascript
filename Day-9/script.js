6cd99140d6f63555939db009d690b3cbbc538163
Sanjeev <coolviccy123@gmail.com>
2024-08-31 23:52:42 +0530

    file added

--- Day-9/script.js
+++ Day-9/script.js
@@ -9,3 +9,10 @@
 // let result = "hello" - 5;
 
 // console.log(result);
+
+// console.log(Math.sqrt(16))
+// console.log(Math.pow(2,4))
+// console.log(Math.floor(2.7))
+// console.log(Math.ceil(2.7))
+// console.log(Math.round(2.7))
+// console.log(Math.random(2.7))
