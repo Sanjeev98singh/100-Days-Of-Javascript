cc9b7e2aee618d5c17fd1e9cdb377e408a4f0b75
Sanjeev <coolviccy123@gmail.com>
2024-09-02 16:57:41 +0530

    diffrence between == and === file added

--- Day-_11/script.js
+++ Day-_11/script.js
@@ -1,4 +1,9 @@
-let userAge1 = 25
+// let userAge1 = 25
+// let userAge2 = 27
+
+let userAge1 = '27'
 let userAge2 = 27
 
 // console.log(userAge1 == userAge2) false
+// console.log(userAge1 == userAge2) true
+console.log(userAge1 == userAge2) 
